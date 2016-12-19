c1bd16dfb24c9e9a791a540aee5f3e261eacc993
Yuriy Voina <yurisson7@gmail.com>
2016-12-19 20:47:56 +0200

    added files

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,6 @@
+﻿public class MyFirstProgram {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
